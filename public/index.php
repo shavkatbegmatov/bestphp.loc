c3709e6d4aa895a760e64c8a833a998c46771bc2
SHAVKAT BEGMATOV <user53-145@qqb.local>
2025-04-07 14:25:06 +0500

    маршруты с параметрами

--- public/index.php
+++ public/index.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once __DIR__ . '/../vendor/autoload.php';
+// require_once __DIR__ . '/../vendor/autoload.php';
 
 use App\Router;
 use App\Repository\UserRepository;
@@ -34,9 +34,29 @@ $router->get('/roles', function () {
     return json_encode($repo->all());
 });
 
+$router->get('/roles/{id}', function ($id) {
+    $repo = new RoleRepository();
+    $role = $repo->find((int) $id);
+    if (!$role) {
+        http_response_code(404);
+        return json_encode(['error' => 'Role not found']);
+    }
+    return json_encode($role);
+});
+
 $router->get('/permissions', function () {
     $repo = new PermissionRepository();
     return json_encode($repo->all());
+});
+
+$router->get('/permissions/{id}', function ($id) {
+    $repo = new PermissionRepository();
+    $perm = $repo->find((int) $id);
+    if (!$perm) {
+        http_response_code(404);
+        return json_encode(['error' => 'Permission not found']);
+    }
+    return json_encode($perm);
 });
 
 $router->get('/modules', function () {
@@ -44,6 +64,14 @@ $router->get('/modules', function () {
     return json_encode($repo->all());
 });
 
+$router->get('/modules/{id}', function ($id) {
+    $repo = new ModuleRepository();
+    $mod = $repo->find((int) $id);
+    if (!$mod) {
+        http_response_code(404);
+        return json_encode(['error' => 'Module not found']);
+    }
+    return json_encode($mod);
+});
+
 $router->dispatch($_SERVER['REQUEST_METHOD'], $_SERVER['REQUEST_URI']);
-// http_response_code(404);
-// echo "404 Not Found";
